0de28d8913dcb758b885f739a24a560dc5713590
rieg-ec <rieg@uc.cl>
2021-10-12 16:53:17 -0300

    feat(telegram-webhook): bot commands

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,7 +1,39 @@
 class TelegramWebhooksController < Telegram::Bot::UpdatesController
   include Telegram::Bot::UpdatesController::MessageContext
+  include Telegram::Bot::UpdatesController::Session
+
+  before_action :street_lookup, except: %i[start! help! register_street!]
+
+  def street_state!(*)
+    respond_with :message, text: t('.content', {
+                                     street: user.street.name,
+                                     occupied: user.street.occupied_slots,
+                                     total: user.street.parking_slots
+                                   })
+  end
+
+  def car_arrived!(*args)
+    args << 1 unless args.any?
+    user.street.increase_occupied_slots(args.first.to_i)
+    respond_with :message, text: t('.success')
+  end
+
+  def car_left!(*args)
+    args << 1 unless args.any?
+    user.street.decrease_occupied_slots(args.first.to_i)
+    respond_with :message, text: t('.success')
+  end
+
+  def register_street!(*args)
+    return respond_with :message, text: t('.usage') unless args.any?
+
+    street_str = args.join(' ')
+    user.update(street: street(street_str))
+    respond_with :message, text: t('.success')
+  end
 
   def start!(*)
+    TelegramUser.find_or_create_by(telegram_id: chat['id'])
     respond_with :message, text: t('.content')
   end
 
@@ -9,97 +41,25 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     respond_with :message, text: t('.content')
   end
 
-  def memo!(*args)
-    if args.any?
-      session[:memo] = args.join(' ')
-      respond_with :message, text: t('.notice')
-    else
-      respond_with :message, text: t('.prompt')
-      save_context :memo!
-    end
+  private
+
+  def street_lookup
+    respond_with :message, text: t('telegram_webhooks.street_lookup.missing_street') unless user.street.present?
   end
 
-  def remind_me!(*)
-    to_remind = session.delete(:memo)
-    reply = to_remind || t('.nothing')
-    respond_with :message, text: reply
+  def street(name)
+    @street ||= Street.find_or_create_by!(name: name)
   end
 
-  def keyboard!(value = nil, *)
-    if value
-      respond_with :message, text: t('.selected', value: value)
-    else
-      save_context :keyboard!
-      respond_with :message, text: t('.prompt'), reply_markup: {
-        keyboard: [t('.buttons')],
-        resize_keyboard: true,
-        one_time_keyboard: true,
-        selective: true,
-      }
-    end
+  def user
+    @user ||= TelegramUser.find_or_create_by!(telegram_id: telegram_id)
   end
 
-  def inline_keyboard!(*)
-    respond_with :message, text: t('.prompt'), reply_markup: {
-      inline_keyboard: [
-        [
-          {text: t('.alert'), callback_data: 'alert'},
-          {text: t('.no_alert'), callback_data: 'no_alert'},
-        ],
-        [{text: t('.repo'), url: 'https://github.com/telegram-bot-rb/telegram-bot'}],
-      ],
-    }
-  end
-
-  def callback_query(data)
-    if data == 'alert'
-      answer_callback_query t('.alert'), show_alert: true
-    else
-      answer_callback_query t('.no_alert')
-    end
-  end
-
-  def inline_query(query, _offset)
-    query = query.first(10) # it's just an example, don't use large queries.
-    t_description = t('.description')
-    t_content = t('.content')
-    results = Array.new(5) do |i|
-      {
-        type: :article,
-        title: "#{query}-#{i}",
-        id: "#{query}-#{i}",
-        description: "#{t_description} #{i}",
-        input_message_content: {
-          message_text: "#{t_content} #{i}",
-        },
-      }
-    end
-    answer_inline_query results
-  end
-
-  # As there is no chat id in such requests, we can not respond instantly.
-  # So we just save the result_id, and it's available then with `/last_chosen_inline_result`.
-  def chosen_inline_result(result_id, _query)
-    session[:last_chosen_inline_result] = result_id
-  end
-
-  def last_chosen_inline_result!(*)
-    result_id = session[:last_chosen_inline_result]
-    if result_id
-      respond_with :message, text: t('.selected', result_id: result_id)
-    else
-      respond_with :message, text: t('.prompt')
-    end
+  def telegram_id
+    @telegram_id ||= chat['id']
   end
 
   def message(message)
     respond_with :message, text: t('.content', text: message['text'])
-  end
-
-  def action_missing(action, *_args)
-    if action_type == :command
-      respond_with :message,
-        text: t('telegram_webhooks.action_missing.command', command: action_options[:command])
-    end
   end
 end
